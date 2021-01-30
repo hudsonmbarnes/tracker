ab72c444eb364ed53af8732cee91832821ba1848
hudsonmbarnes <hudsonmbarnes@gmail.com>
2021-01-29 20:23:53 -0800

    added switch cases

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const conTab = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -9,3 +10,76 @@ const connection = mysql.createConnection({
     password: "password",
     database: "employees_db"
   });
+
+console.log(`╔═════════════════════════════════════════════════════╗
+║                                                     ║
+║     _____                 _                         ║
+║    | ____|_ __ ___  _ __ | | ___  _   _  ___  ___   ║
+║    |  _| | '_ \` _ \\| '_ \\| |/ _ \\| | | |/ _ \\/ _ \\  ║
+║    | |___| | | | | | |_) | | (_) | |_| |  __/  __/  ║
+║    |_____|_| |_| |_| .__/|_|\\___/ \\__, |\\___|\\___|  ║
+║                    |_|            |___/             ║
+║                                                     ║
+║     __  __                                          ║
+║    |  \\/  | __ _ _ __   __ _  __ _  ___ _ __        ║
+║    | |\\/| |/ _\` | '_ \\ / _\` |/ _\` |\/ _ \\ '__|       ║
+║    | |  | | (_| | | | | (_| | (_| |  __/ |          ║
+║    |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|          ║
+║                              |___/                  ║
+║                                                     ║
+\╚═════════════════════════════════════════════════════╝
+`);
+
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+  
+// user choices
+function start(){
+    inquirer
+        .prompt({
+            name: "choices",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["ADD DEPARTMENT", "ADD ROLE", "ADD EMPLOYEE", "VIEW DEPARTMENT", "VIEW ROLE", "VIEW EMPLOYEE", "UPDATE EMPLOYEE"]
+        })
+
+        .then(function(answer){
+            switch (answer.action) {
+                case "ADD DEPARTMENT":
+                addDepartment();
+                break;
+                
+                case "ADD ROLE":
+                addRole();
+                break;
+                
+                case "ADD EMPLOYEE":
+                addEmployee();
+                break;
+
+                case "VIEW DEPARTMENT":
+                viewDepartment();
+                break;
+
+                case "VIEW ROLE":
+                viewRole();
+                break;
+
+                case "VIEW EMPLOYEE":
+                viewEmployee();
+                break;
+
+                case "UPDATE EMPLOYEE":
+                updateEmployee();
+                break;
+
+                default:
+                end();
+            }
+        });
+}
+
+function addDepartment() {
+    
